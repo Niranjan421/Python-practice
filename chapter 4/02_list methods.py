f1bae03a248b1972b0f7c2eb8fdd5f58678f987e
Niranjan Jha <116334937+Niranjan421@users.noreply.github.com>
2024-10-12 13:11:30 +0530

    Add files via upload

---
+++ chapter 4/02_list methods.py
@@ -0,0 +1,12 @@
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+print(friends)
+friends.append("Niranjan")
+print(friends)
+
+l1=[1,34,62,2,6,11]
+# l1.sort()
+# l1.reverse()
+# l1.insert(3,33333) # Insert 33333 such that its index in the list is 
+# l1.pop(3)
+l1.remove(62)
+print(l1)
