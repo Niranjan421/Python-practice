e257458316e3fe5e38be3f12dd495689ef92ee13
Niranjan Jha <116334937+Niranjan421@users.noreply.github.com>
2024-10-12 13:13:11 +0530

    Add files via upload

---
+++ chapter 4/problem2.py
@@ -0,0 +1,17 @@
+Marks=[]
+
+M1=int(input("Enter Marks Here: "))
+Marks.append(M1)
+M2=int(input("Enter Marks Here: "))
+Marks.append(M2)
+M3=int(input("Enter Marks Here: "))
+Marks.append(M3)
+M4=int(input("Enter Marks Here: "))
+Marks.append(M4)
+M5=int(input("Enter Marks Here: "))
+Marks.append(M5)
+M6=int(input("Enter Marks Here: "))
+Marks.append(M6)
+
+Marks.sort()
+print(Marks)
