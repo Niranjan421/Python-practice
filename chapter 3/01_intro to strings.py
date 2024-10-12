79b3833b373b59abe0d06d5769c9b9cfc3dd215c
Niranjan Jha <116334937+Niranjan421@users.noreply.github.com>
2024-10-12 13:16:03 +0530

    Add files via upload

---
+++ chapter 3/01_intro to strings.py
@@ -0,0 +1,8 @@
+name ="Niranjan"
+# print(len(name))
+
+nameshorts= name[0:3] # start from  index 0 all the way till 3 (excluding 3)
+print(nameshorts)
+
+character1=name[1]
+print(character1)
---
+++ chapter 3/03_str functions.py
@@ -0,0 +1,8 @@
+name ="niranjan"
+ 
+print(len(name))
+
+print(name.endswith("njan"))
+print(name.startswith("nira"))
+print(name.capitalize()) #starting wali string ke first letter ko capital kar deta hai 
+print(name.replace("niranjan","Coder"))
