401dcf0c302ed20e629221195a3b599d3c14e842
Niranjan Jha <116334937+Niranjan421@users.noreply.github.com>
2024-10-18 11:26:39 +0530

    Add files via upload

---
+++ chapter 6/program7.py
@@ -0,0 +1,9 @@
+post = input("Enter the post: ")
+
+
+
+if("Niranjan".lower in post.lower()):
+    print("This post is talking about harry")
+
+else:
+    print("This post is not talking about harry")
